e5cf993573772dd1371b7684f9e6ea518529d4d8
Oliver Kenyon <okenyon@scottlogic.com>
2017-10-12 10:28:01 +0100

    Change config to read from environment variables instead of being hard-coded

--- src/main/java/com/company/Config.java
+++ src/main/java/com/company/Config.java
@@ -2,18 +2,29 @@ package com.company;
 
 public class Config {
     public static String kafkaHost() {
-        return System.getenv("WM_KAFKA_HOST");
+        return getEnvironmentVariable("WM_KAFKA_HOST", "localhost");
     }
 
     public static String graphiteHostName() {
-        return "localhost";
+        return getEnvironmentVariable("WM_GRAPHITE_HOST_NAME", "localhost");
     }
 
     public static Integer graphitePort() {
-        return 2004;
+        String portString = getEnvironmentVariable("WM_GRAPHITE_PORT", "2004");
+        return Integer.parseInt(portString);
     }
 
     public static Boolean startFromBeginning() {
         return "true".equals(System.getenv("WM_GRAPHITE_FROM_BEGINNING"));
     }
+
+    private static String getEnvironmentVariable(String name, String defaultValue) {
+        String value = System.getenv(name);
+        if (value == null || "".equals(value)) {
+            return defaultValue;
+        }
+
+        return value;
+    }
+
 }
